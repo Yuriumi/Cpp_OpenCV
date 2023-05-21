f89314f02866660aa176e73109d49e68eceecb4b
shz2002 <sj1050437739@gmail.com>
2023-05-21 21:07:22 +0800

    02-图像的读取,显示和保存

--- LearnOpenCV_01/LearnOpenCV_01/src/main.cpp
+++ LearnOpenCV_01/LearnOpenCV_01/src/main.cpp
@@ -1,12 +1,25 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 int main()
 {
-	cv::Mat img = cv::imread("./img/test.jpg");
+	cv::Mat image;	// 创建一个空图像
 
-	cv::imshow("img", img);
+	image = cv::imread("./img/kawaii.jpg");	// 读取图像文件
 
-	cv::waitKey();
+	// 检测读取是否成功
+	if (image.empty())
+	{
+		std::cout << "ERROR : Read image Failed!\n";
+		return -1;
+	}
+
+	cv::namedWindow("Image");	// 创建图片显示窗口,可不写
+	cv::imshow("Image", image);	// 在窗口显示图片
+
+	cv::imwrite("./img/1.png", image);	// 将图像保存为png格式
+
+	cv::waitKey(0);
 
 	return 0;
 }
